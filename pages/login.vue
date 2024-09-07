15be5aa457b54338c8a82f15c26e0a0a765bf7fc
Harienne Jay Ranches <harvy.ran228@gmail.com>
2024-09-07 23:44:12 +0800

    changes in landing page3

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col min-h-screen bg-gray-100">
     <!-- Main content -->
     <div class="flex-grow">
       <!-- Your existing content here -->
@@ -15,7 +15,7 @@
         <div class="flex justify-between items-center px-10 h-full">
           <img src="/public/images/auctionwave-logo-final.png" class="h-10 object-contain" alt="Logo" />
           <div class="flex items-center space-x-4">
-            <div class="px-5 py-2 font-medium text-white bg-custom-bluegreen rounded-md cursor-pointer flex items-center justify-center" @click="toggleForm">Sign in</div>
+            <div class="px-5 py-2 font-medium text-white bg-custom-bluegreen rounded-md cursor-pointer flex items-center justify-center hover:bg-white hover:text-custom-bluegreen hover:border hover:border-custom-bluegreen" @click="toggleForm">Sign in</div>
           </div>
         </div>
       </nav>
@@ -100,146 +100,170 @@
 
 <div class="flex justify-between items-center w-full  bg-custom-bluegreen">
     <!-- Text Container (50% width on the left) -->
-    <div class="w-1/2 text-white max-w-md text-left ml-44 animate-slide-in-left">
-        <h1 class="text-6xl font-medium mb-4 text-white">Online Auctions Made Easy</h1>
-        <p class="text-xl text-white mb-6">Experience a seamless and convenient way to participate in online auctions, from anywhere and at any time.</p>
+    <div class="w-full text-white max-w-xl text-left ml-44 animate-slide-in-left">
+        <h1 class="text-7xl font-bold mb-4 text-white">Online Auctions Made Easy</h1>
+        <div class ="full">
+          <p class="text-2xl font-semi-bold text-white mb-6">Experience a seamless and convenient way to participate in online auctions, from anywhere and at any time.</p>
+        </div>
         
-        <button class="px-6 py-3 bg-green-500 text-white font-semibold rounded-md hover:bg-opacity-90 transition duration-300">
-          Get Started
+        
+        <button class="px-6 py-3 bg-green-500 text-white font-semibold rounded-md hover:bg-opacity-90 transition duration-300 hover:bg-white hover:text-custom-bluegreen hover:border hover:border-custom-bluegreen">
+          Start Browsing
         </button>
     </div>
 
     <!-- Image Container (50% width on the right) -->
-    <div class="w-1/2 flex justify-center items-center">
-        <img src="/public/images/22062.jpg" alt="Image" class="w-full h-fit">
+    <div class="w-1/2 flex justify-center items-center ">
+        <img src="/public/images/5989.jpg" alt="Image" class="w-full h-fit rounded-2xl">
     </div>
 </div>
 
 <!------------------------------------------------------------->
-<div class="mx-auto justify-center items-center w-3/5 my-24">
-    <div class="flex justify-center">
-        <div class=" mx-4 w-3/5">
-            <img class="w-24 h-24 " src="../public/images/usermanage.png">
-            <div class="mt-2">
-                Users are provided with the option to choose whether they want to register as an Auctioneer or a Bidder.
-            </div>
-        </div>
-        <div class=" mx-4 w-3/5">
-            <img class="w-24 h-24 " src="../public/images/CMS.png">
-            <div class="mt-2">
-              a user-friendly interface specifically designed for auctioneers to create new auction listings.
-            </div>
-        </div>
-        <div class=" mx-4 w-3/5">
-            <img class="w-24 h-24 " src="../public/images/Transaction.png">
-            <div class="mt-2">
-              bidders have the option to place bids on auction listings with different bidding types, including lowest bid and offer-type.
-            </div>
-        </div>
-    </div>
-    <div class="flex justify-center mt-4">
-        <div class=" mx-4 w-1/3">
-            <img class="w-24 h-24 " src="../public/images/payment.png">
-            <div class="mt-2">
-              The transaction is facilitated by payment gateways or processors, 
-              which securely transmit payment information, such as credit or debit card details, bank account information, or digital wallet credentials.
-            </div>
-        </div>
-        <div class=" mx-4 w-1/3">
-            <img class="w-24 h-24 " src="../public/images/admin.png">
-            <div class="mt-2">
-              The platform provides administrators with dependable user management tools, allowing them to track and monitor user account activities, 
-              including registrations, updates, logs and suspensions.
 
-            </div>
-        </div>
-    </div>
-</div>
 
 
 <!------------------------------------------------------------->
 
-<div class="text-4xl font-medium mx-auto mt-32">Products Offered</div>
-<div class="mx-auto justify-center items-center w-3/5 mt-24 mb-10">
+<div class="mx-auto w-9/12 bg-white my-12 rounded-2xl">
+  <div class="text-6xl font-bold w-full mt-32 text-custom-bluegreen drop-shadow-3xl text-center">Products Offered</div>
+<div class="mx-auto justify-center items-center w-10/12 mt-24 mb-10 transform transition-all hover:-translate-y-2 duration-300 hover:scale-105 ">
     <div class="flex justify-center">
         <div class="text-center  w-3/5">
-            <div class="mt-2 rounded-md overflow-hidden">
+            <div class="mt-2 rounded-md overflow-hidden hover:drop-shadow-2xl ">
               <img src="public/images/art.jpg" alt="Image 1" class="w-full h-full object-cover">
             </div>
-        </div>
+        </div>  
         <div class="w-3/5 mt-4">  
-            <div class="bg-custom-bluegreen my-24 py-16 rounded-r-lg">
+            <div class="bg-custom-bluegreen my-24 py-16 rounded-r-lg hover:drop-shadow-2xl">
               <h1 class="text-3xl font-medium text-white mx-14">Art</h1>
-                <p class="mx-14 text-white">Auction items include paintings, sculptures, and other creative works.</p> 
+                <p class="mx-14 text-white text-lg">Auction items include paintings, sculptures, and other creative works.</p> 
             </div>
            
         </div>
     </div>
 </div>
 
-<div class="mx-auto justify-center items-center w-3/5 mb-10 my-24">
+<div class="mx-auto justify-center items-center w-10/12 mb-10 my-24 transform transition-all hover:-translate-y-2 duration-300 hover:scale-105 ">
     <div class="flex justify-center">
       <div class=" w-3/5 mt-4">  
-        <div class="bg-custom-bluegreen my-24 py-16 rounded-l-lg">
+        <div class="bg-custom-bluegreen my-24 py-16 rounded-l-lg hover:drop-shadow-2xl">
               <h1 class="text-3xl font-medium text-white mx-14">furniture</h1>
-                <p class="mx-14 text-white">Browse a diverse selection of furniture, from modern to antique pieces.</p> 
+                <p class="mx-14 text-white text-lg">Browse a diverse selection of furniture, from modern to antique pieces.</p> 
             </div>  
         </div>
         <div class="text-center  w-3/5">
-            <div class="mt-2 rounded-md overflow-hidden">
+            <div class="mt-2 rounded-md overflow-hidden hover:drop-shadow-2xl">
               <img src="public/images/furniture.jpg" alt="Image 1" class="w-full h-full object-cover">
             </div>
         </div>
     </div>
 </div>
 
-<div class="mx-auto justify-center items-center w-3/5 mb-10 my-24">
+<div class="mx-auto justify-center items-center w-10/12 mb-10 my-24 transform transition-all hover:-translate-y-2 duration-300 hover:scale-105">
     <div class="flex justify-center">
         <div class="text-center  w-3/5">
-            <div class="mt-2 rounded-md overflow-hidden">
+            <div class="mt-2 rounded-md overflow-hidden hover:drop-shadow-2xl">
               <img src="public/images/electronics.jpg" alt="Image 1" class="w-full h-full object-cover">
             </div>
         </div>
         <div class=" w-3/5 mt-4">  
             <div class="bg-custom-bluegreen my-24 py-16 rounded-r-lg">
               <h1 class="text-3xl font-medium text-white mx-14 ">Electronics</h1>
-                <p class="mx-14 text-white">Auction items cover gadgets like smartphones, laptops, and audio equipment.</p> 
+                <p class="mx-14 text-white text-lg">Auction items cover gadgets like smartphones, laptops, and audio equipment.</p> 
             </div>  
         </div>
     </div>
 </div>
 
-<div class="mx-auto justify-center items-center w-3/5 mb-10 my-24">
+<div class="mx-auto justify-center items-center w-10/12 mb-10 my-24 transform transition-all hover:-translate-y-2 duration-300 hover:scale-105 ">
     <div class="flex justify-center">
       <div class=" w-3/5 mt-4">  
             <div class="bg-custom-bluegreen my-24 py-16 rounded-l-lg">
               <h1 class="text-3xl font-medium text-white mx-14">Collectibles</h1>
-                <p class="mx-14 text-white">Explore rare and valuable items such as coins, stamps, and memorabilia. Each piece is selected for its uniqueness.</p> 
+                <p class="mx-14 text-white text-lg">Explore rare and valuable items such as coins, stamps, and memorabilia. Each piece is selected for its uniqueness.</p> 
             </div>  
       </div>
         <div class="text-center  w-3/5">
-            <div class="mt-2 rounded-md overflow-hidden">
+            <div class="mt-2 rounded-md overflow-hidden hover:drop-shadow-2xl">
               <img src="public/images/collectibles.jpg" alt="Image 1" class="w-full h-full object-cover">
             </div>
         </div>
     </div>
 </div>
 
-<div class="mx-auto justify-center items-center w-3/5 my-24 ">
+<div class="mx-auto justify-center items-center w-10/12 my-24 transform transition-all hover:-translate-y-2 duration-300 hover:scale-105 ">
     <div class="flex justify-center">
         <div class="text-center w-3/5">
-            <div class="mt-2 rounded-md overflow-hidden">
+            <div class="mt-2 rounded-md overflow-hidden hover:drop-shadow-2xl">
               <img src="public/images/realestate.jpg" alt="Image 1" class="w-full h-full object-cover">
             </div>
         </div>
         <div class=" w-3/5 mt-4">  
-            <div class="bg-custom-bluegreen my-24 py-16 rounded-r-lg">
+            <div class="bg-custom-bluegreen my-24 py-16 rounded-r-lg ">
               <h1 class="text-3xl font-medium text-white mx-14">Real estate</h1>
-                <p class="mx-14 text-white">Bid on a range of properties, including homes, commercial buildings, and land. Each listing provides detailed information</p> 
+                <p class="mx-14 text-white text-lg">Bid on a range of properties, including homes, commercial buildings, and land. Each listing provides detailed information</p> 
             </div>  
         </div>
     </div>
 </div>
+
+
+
+<!----------------------------------------------------------------------------------->
+
+
+</div>
+
+<div class="w-9/12 bg-white mb-12 rounded-2xl mx-auto">
+  <div class="text-6xl font-bold w-full my-24 text-custom-bluegreen drop-shadow-3xl text-center">How It Works</div>
+    <div class="mx-auto justify-center items-center w-5/6 my-24">
+      <div class="flex justify-center">
+          <div class=" mx-4 w-3/5 my-4">
+              <img class="w-24 h-24 mx-auto" src="../public/images/usermanage.png">
+              <div class="mt-2 text-center">
+                  Users are provided with the option to choose whether they want to register as an Auctioneer or a Bidder.
+              </div>
+          </div>
+          <div class=" mx-4 w-3/5 my-4">
+              <img class="w-24 h-24 mx-auto" src="../public/images/CMS.png">
+              <div class="mt-2 text-center">
+                a user-friendly interface specifically designed for auctioneers to create new auction listings.
+              </div>
+          </div>
+          <div class=" mx-4 w-3/5 my-4">
+              <img class="w-24 h-24 mx-auto" src="../public/images/Transaction.png">
+              <div class="mt-2 text-center">
+                bidders have the option to place bids on auction listings with different bidding types, including lowest bid and offer-type.
+              </div>
+          </div>
+      </div>
+      <div class="flex justify-center mt-4">
+          <div class=" mx-4 w-1/3 my-4">
+              <img class="w-24 h-24 mx-auto" src="../public/images/payment.png">
+              <div class="mt-2 text-center">
+                The transaction is facilitated by payment gateways or processors, 
+                which securely transmit payment information, such as credit or debit card details.
+              </div>
+          </div>
+          <div class=" mx-4 w-1/3 my-4">
+              <img class="w-24 h-24 mx-auto" src="../public/images/admin.png">
+              <div class="mt-2 text-center">
+                The platform provides administrators with dependable user management tools, allowing them to track and monitor user account activities.
+              </div>
+          </div>
+      </div>
+</div>
+</div>
+
+<!----------------------------------------------------------------------------------->
+
+
+<!--------------------------------------------------------------->
+    
+
+
+
+
 
 
 
@@ -308,5 +332,26 @@ function prevImage() {
 /* Styles for the overlay */
 .bg-gray-900 {
   background-color: rgba(0, 0, 0, 0.5); /* Adjust opacity as needed */
+}
+
+/* assets/css/custom.css */
+.magnify-container {
+    position: relative;
+    overflow: hidden;
+}
+.magnify-image {
+    transition: transform 0.3s ease;
+}
+.magnify-container:hover .magnify-image {
+    transform: scale(1.5); /* Adjust zoom level as needed */
+}
+.magnify-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.3);
+    pointer-events: none;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -43,6 +43,13 @@ export default {
       animation: {
         'slide-in-left': 'slideInLeft 0.5s ease-out forwards',
       },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      },
 
 
     },
